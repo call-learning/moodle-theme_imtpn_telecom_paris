050d5bb6425b0795c65c16791180e5faf8345bbe
Laurent David <lmedavid@gmail.com>
2023-07-30 12:42:13 +0200

    Fix issue with guest login and add new svg logos

--- lib.php
+++ lib.php
@@ -22,6 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use theme_imtpn\local\utils;
 use theme_imtpn\mur_pedagogique;
 
 /**
@@ -42,7 +43,6 @@ function theme_imtpn_telecom_paris_pluginfile($course, $cm, $context, $filearea,
         $options);
 }
 
-
 /**
  * Fix issue with notloggedin class
  *
@@ -53,5 +53,5 @@ function theme_imtpn_telecom_paris_pluginfile($course, $cm, $context, $filearea,
  * @param $page
  */
 function theme_imtpn_telecom_paris_page_init($page) {
-    mur_pedagogique::set_additional_page_classes($page);
+    utils::add_body_additional_classes($page);
 }
